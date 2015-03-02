aa52fcfe5dfdd665d2ef02ea3a4e7189bd65cac1
Chris Moyer <cmoyer@newstex.com>
2015-03-02 18:43:00 -0500

    Added mobile support

--- background.js
+++ background.js
@@ -1,8 +1,8 @@
 chrome.app.runtime.onLaunched.addListener(function() {
 	chrome.app.window.create('index.html', {
 		bounds: {
-			width: 1536,
-			height: 1280,
+			width: 320,
+			height: 568,
 		}
 	});
 });
